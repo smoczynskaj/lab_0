f204030eb9555ed60881db96958b653cbef75d9f
smoczynska.justyna <smoczynska.justyna@gmail.com>
2017-11-25 17:33:33 +0100

    Zmodyfikowane, testy przechodzą

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,10 @@ import java.util.List;
 public class TailsGenerator {
 
 	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
+		List<String> tails = new ArrayList<>(value.length());
+		for (int i = 0; i <= value.length(); i++) {
+			tails.add(value.substring(i));
+		}
 		return tails;
 	}
 
